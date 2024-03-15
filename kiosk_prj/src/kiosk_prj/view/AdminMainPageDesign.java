aff990060dd94b33a2137e900a2ed66dc2fde586
user <user@DESKTOP-G3FF7S5>
2024-03-15 17:49:03 +0900

    로그인 화면 구현

---
+++ kiosk_prj/src/kiosk_prj/view/AdminMainPageDesign.java
@@ -0,0 +1,22 @@
+package kiosk_prj.view;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class AdminMainPageDesign extends JFrame {
+	
+	
+	public AdminMainPageDesign() {
+		super("");
+		
+		
+		//기타 설정 등등등드읃으등ㄷ
+		setVisible(true);
+		setResizable(false);//창 크기 변경 불가능
+		setBounds(455,130,1024,768);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}//AdminMainPageDesign
+	
+	
+}//class
