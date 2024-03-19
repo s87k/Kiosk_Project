a52cdd6a38a981fa76e26245c1d937bdf36b97ab
choiyeon <2dpaesu@gmail.com>
2024-03-20 02:22:19 +0900

    yatta

--- kiosk_prj/src/kiosk_prj/view/OrderDesign.java
+++ kiosk_prj/src/kiosk_prj/view/OrderDesign.java
@@ -1,213 +1,165 @@
 package kiosk_prj.view;
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
-import kiosk_prj.controller.OrderEvent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-@SuppressWarnings("serial")
 public class OrderDesign extends JFrame implements ActionListener {
-	
-	private JPanel menu;
-	private JButton pay, cancel, best, coffee, nonCoffee, tea, smoothie;
-	private JButton espresso, americano, cafeLatte, vanillaLatte;
-	private JList<String> cartList;
-	private JLabel orderPrice;
+	private JPanel menueBarPanel, cardPanel, cartPanel;
+	private JButton best, coffee, nonCoffee, tea, smoothie, pay, cancel;
+	private JPanel panel1, panel2, panel3, panel4, panel5;
+	private JTable cartList;
+	private JLabel jlOrderPrice;
+	private JTextField jtfOrderPrice;
 	private CardLayout cardLayout;
-
-	private DefaultListModel<String> dlmCartList;
+	private DefaultTableModel dtmCartList;
 
 	public OrderDesign() {
 		setTitle("Order Page");
+		setSize(600, 800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
 
-		// CardLayout 생성
-		cardLayout = new CardLayout();
-		menu = new JPanel(cardLayout);
-
-		// 1 번째 패널 생성
-		JPanel panel1 = new JPanel();
-		panel1.setLayout(null);
-
-		JLabel jl1 = new JLabel("베스트 메뉴입니다.");
-		jl1.setBounds(240, 3, 150, 30);
-		panel1.add(jl1);
-
-		// 베스트 메뉴 버튼 생성 및 추가
-		espresso = new JButton("Espresso");
-		espresso.setBounds(130, 40, 150, 150);
-		panel1.add(espresso);
-
-		americano = new JButton("Americano");
-		americano.setBounds(310, 40, 150, 150);
-		panel1.add(americano);
-
-		cafeLatte = new JButton("Cafe Latte");
-		cafeLatte.setBounds(130, 220, 150, 150);
-		panel1.add(cafeLatte);
-
-		vanillaLatte = new JButton("Vanilla Latte");
-		vanillaLatte.setBounds(310, 220, 150, 150);
-		panel1.add(vanillaLatte);
-/////////////////////////////////////////////////////////////////////
-		// 2 번째 패널 생성
-		JPanel panel2 = new JPanel();
-		panel2.setLayout(null);
-
-		JLabel jl2 = new JLabel("커피 메뉴입니다.");
-		jl2.setBounds(240, 3, 150, 30);
-		panel2.add(jl2);
-
-		// 커피 메뉴 버튼 생성 및 추가
-		espresso = new JButton("Espresso");
-		espresso.setBounds(130, 40, 150, 150);
-		panel2.add(espresso);
-
-		americano = new JButton("Americano");
-		americano.setBounds(310, 40, 150, 150);
-		panel2.add(americano);
-
-		cafeLatte = new JButton("Cafe Latte");
-		cafeLatte.setBounds(130, 220, 150, 150);
-		panel2.add(cafeLatte);
-
-		vanillaLatte = new JButton("Vanilla Latte");
-		vanillaLatte.setBounds(310, 220, 150, 150);
-		panel2.add(vanillaLatte);
-
-		// 장바구니 JList 생성 및 추가
-		dlmCartList = new DefaultListModel<String>();
-		cartList = new JList<String>(dlmCartList);
-		cartList.setBounds(75, 410, 450, 200);
-		panel2.add(cartList);
-
-		// 주문금액 JLabel 생성 및 추가
-		orderPrice = new JLabel("Order Price: 1200원");
-		orderPrice.setBounds(85, 615, 200, 50);
-		panel2.add(orderPrice);
-
-		// 결제 JButton 생성 및 추가
-		pay = new JButton("결제");
-		pay.setBounds(425, 630, 100, 50); // 285
-		panel2.add(pay);
-
-		// 취소 JButton 생성 및 추가
-		cancel = new JButton("취소");
-		cancel.setBounds(315, 630, 100, 50);
-		panel2.add(cancel);
-/////////////////////////////////////////////////////////////////////
-		// 3 번째 패널 생성
-		JPanel panel3 = new JPanel();
-		panel3.add(new JLabel("논커피 메뉴입니다."));
-
-		// 4 번째 패널 생성
-		JPanel panel4 = new JPanel();
-		panel4.add(new JLabel("티 메뉴입니다."));
-
-		// 5 번째 패널 생성
-		JPanel panel5 = new JPanel();
-		panel5.add(new JLabel("스무디 메뉴입니다."));
-
-		// 장바구니 JList 생성 및 추가
-		dlmCartList = new DefaultListModel<String>();
-		cartList = new JList<String>(dlmCartList);
-		cartList.setBounds(75, 410, 450, 200);
-		panel1.add(cartList);
-
-		// 주문금액 JLabel 생성 및 추가
-		orderPrice = new JLabel("Order Price: 1200원");
-		orderPrice.setBounds(85, 615, 200, 50);
-		panel1.add(orderPrice);
-
-		// 결제 JButton 생성 및 추가
-		pay = new JButton("결제");
-		pay.setBounds(425, 630, 100, 50); // 285
-		panel1.add(pay);
-
-		// 취소 JButton 생성 및 추가
-		cancel = new JButton("취소");
-		cancel.setBounds(315, 630, 100, 50);
-		panel1.add(cancel);
-
-		// 메뉴바 버튼 패널 생성
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new FlowLayout());
-
+		// 첫 번째 JPanel (상단)
+		menueBarPanel = new JPanel();
 		best = new JButton("Best");
-		coffee = new JButton("Coffee");
-		nonCoffee = new JButton("Non Coffee");
-		tea = new JButton("Tea");
-		smoothie = new JButton("Smoothie");
-
-		buttonPanel.add(best);
-		buttonPanel.add(coffee);
-		buttonPanel.add(nonCoffee);
-		buttonPanel.add(tea);
-		buttonPanel.add(smoothie);
-
-		// 패널들을 cards 패널에 추가
-		menu.add(panel1, "1 Best");
-		menu.add(panel2, "2 Coffee");
-		menu.add(panel3, "3 Non Coffee");
-		menu.add(panel4, "4 Tea");
-		menu.add(panel5, "5 Smoothie");
-
-		// cards 패널을 프레임에 추가
-		getContentPane().add(buttonPanel, BorderLayout.NORTH);
-		getContentPane().add(menu, BorderLayout.CENTER);
-
-		// 패널 addActionListener 추가
 		best.addActionListener(this);
+		coffee = new JButton("Coffee");
 		coffee.addActionListener(this);
+		nonCoffee = new JButton("Non Coffee");
 		nonCoffee.addActionListener(this);
+		tea = new JButton("Tea");
 		tea.addActionListener(this);
+		smoothie = new JButton("Smoothie");
 		smoothie.addActionListener(this);
+		menueBarPanel.add(best);
+		menueBarPanel.add(coffee);
+		menueBarPanel.add(nonCoffee);
+		menueBarPanel.add(tea);
+		menueBarPanel.add(smoothie);
+		add(menueBarPanel, BorderLayout.NORTH);
 
-		espresso.addActionListener(this);
-		// OrderEvent에 addActionListener 추가
-		OrderEvent oe = new OrderEvent(this);
+		// 두 번째 JPanel (중앙, CardLayout)
+		cardPanel = new JPanel();
+		cardLayout = new CardLayout();
+		cardPanel.setLayout(cardLayout);
 
-		americano.addActionListener(oe);
-		cafeLatte.addActionListener(oe);
-		vanillaLatte.addActionListener(oe);
+		panel1 = new JPanel();
+		panel1.setLayout(new GridLayout(2, 2));
+		panel1.add(new JButton("베스트 1"));
+		panel1.add(new JButton("베스트 2"));
+		panel1.add(new JButton("베스트 3"));
 
-		pay.addActionListener(oe);
-		cancel.addActionListener(oe);
+		panel2 = new JPanel();
+		panel2.setLayout(new GridLayout(2, 2));
+		panel2.add(new JButton("coffee-1"));
+		panel2.add(new JButton("coffee-2"));
+		panel2.add(new JButton("coffee-3"));
+		panel2.add(new JButton("coffee-4"));
 
-		setVisible(true);
-		setBounds(100, 0, 600, 800);
-	}// OrderDesign
+		panel3 = new JPanel();
+		panel3.setLayout(new GridLayout(2, 2));
+		panel3.add(new JButton("nonCoffee-1"));
+		panel3.add(new JButton("nonCoffee-2"));
+		panel3.add(new JButton("nonCoffee-3"));
+		panel3.add(new JButton("nonCoffee-4"));
+
+		panel4 = new JPanel();
+		panel4.setLayout(new GridLayout(2, 2));
+		panel4.add(new JButton("tea-1"));
+		panel4.add(new JButton("tea-2"));
+		panel4.add(new JButton("tea-3"));
+		panel4.add(new JButton("tea-4"));
+
+		panel5 = new JPanel();
+		panel5.setLayout(new GridLayout(2, 2));
+		panel5.add(new JButton("smoothie-1"));
+		panel5.add(new JButton("smoothie-2"));
+		panel5.add(new JButton("smoothie-3"));
+		panel5.add(new JButton("smoothie-4"));
+
+		cardPanel.add(panel1, "panel1");
+		cardPanel.add(panel2, "panel2");
+		cardPanel.add(panel3, "panel3");
+		cardPanel.add(panel4, "panel4");
+		cardPanel.add(panel5, "panel5");
+
+		add(cardPanel, BorderLayout.CENTER);
+
+		// 세 번째 JPanel (하단)
+		cartPanel = new JPanel();
+		cartPanel.setLayout(new BorderLayout());
+
+//		cartList = new JTable(5, 1);
+		
+		//DefaultTableModel 추가
+		dtmCartList = new DefaultTableModel(new Object[]{"Item"}, 0);
+        cartList = new JTable(dtmCartList);
+		
+		//JTable 장바구니 테두리
+//      JScrollPane scrollPane = new JScrollPane(cartList);
+//      scrollPane.setBorder(BorderFactory.createTitledBorder("장바구니")); // "장바구니" 테두리 추가
+//      cartPanel.add(scrollPane, BorderLayout.CENTER);
+        
+		cartPanel.add(cartList, BorderLayout.CENTER);
+
+		// jlOrderPrice, jtfOrderPrice, cancel, pay 일렬배치
+		JPanel buttonsPanel = new JPanel();
+		jlOrderPrice = new JLabel("주문 금액 : ");
+		buttonsPanel.add(jlOrderPrice);
+
+		jtfOrderPrice = new JTextField(10);
+		jtfOrderPrice.setEditable(false);
+		buttonsPanel.add(jtfOrderPrice);
+
+		cancel = new JButton("취소");
+		buttonsPanel.add(cancel);
+
+		pay = new JButton("결제");
+		buttonsPanel.add(pay);
+
+		cartPanel.add(buttonsPanel, BorderLayout.SOUTH);
+
+		add(cartPanel, BorderLayout.SOUTH);
+
+	}//OrderDesign
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == best) {
-			cardLayout.show(menu, "1 Best");
+			cardLayout.show(cardPanel, "panel1");
 		} else if (e.getSource() == coffee) {
-			cardLayout.show(menu, "2 Coffee");
+			cardLayout.show(cardPanel, "panel2");
 		} else if (e.getSource() == nonCoffee) {
-			cardLayout.show(menu, "3 Non Coffee");
+			cardLayout.show(cardPanel, "panel3");
 		} else if (e.getSource() == tea) {
-			cardLayout.show(menu, "4 Tea");
+			cardLayout.show(cardPanel, "panel4");
 		} else if (e.getSource() == smoothie) {
-			cardLayout.show(menu, "5 Smoothie");
-		} 
-	}// actionPerformed
+			cardLayout.show(cardPanel, "panel5");
+		}
+	}//actionPerformed
 
 	public static void main(String[] args) {
-		new OrderDesign();
-	}// main
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new OrderDesign().setVisible(true);
+			}
+		});
+	}//main
 
-	public JPanel getMenu() {
-		return menu;
+	public JPanel getMenueBarPanel() {
+		return menueBarPanel;
 	}
 
-	public JButton getPay() {
-		return pay;
+	public JPanel getCardPanel() {
+		return cardPanel;
 	}
 
-	public JButton getCancel() {
-		return cancel;
+	public JPanel getCartPanel() {
+		return cartPanel;
 	}
 
 	public JButton getBest() {
@@ -230,36 +182,53 @@ public class OrderDesign extends JFrame implements ActionListener {
 		return smoothie;
 	}
 
-	public JButton getEspresso() {
-		return espresso;
+	public JButton getPay() {
+		return pay;
 	}
 
-	public JButton getAmericano() {
-		return americano;
+	public JButton getCancel() {
+		return cancel;
 	}
 
-	public JButton getCafeLatte() {
-		return cafeLatte;
+	public JPanel getPanel1() {
+		return panel1;
 	}
 
-	public JButton getVanillaLatte() {
-		return vanillaLatte;
+	public JPanel getPanel2() {
+		return panel2;
 	}
 
-	public JList<String> getCartList() {
+	public JPanel getPanel3() {
+		return panel3;
+	}
+
+	public JPanel getPanel4() {
+		return panel4;
+	}
+
+	public JPanel getPanel5() {
+		return panel5;
+	}
+
+	public JTable getCartList() {
 		return cartList;
 	}
 
-	public DefaultListModel<String> getDlmCartList() {
-		return dlmCartList;
+	public JLabel getJlOrderPrice() {
+		return jlOrderPrice;
 	}
 
-	public JLabel getOrderPrice() {
-		return orderPrice;
+	public JTextField getJtfOrderPrice() {
+		return jtfOrderPrice;
 	}
 
 	public CardLayout getCardLayout() {
 		return cardLayout;
 	}
 
-}// class
+	public DefaultTableModel getDtmCartList() {
+		return dtmCartList;
+	}
+	
+	
+}//class
